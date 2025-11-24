af6d08d1125d8fc68fe056f135f65672d9143365
Junior Valera <detoprox@gmail.com>
2025-11-24 00:55:56 -0400

    controlador de Lotes con scaffolding

---
+++ AlmacenVencimientos/Controllers/LotesController.cs
@@ -0,0 +1,176 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AlmacenVencimientos.Data;
+using AlmacenVencimientos.Models;
+
+namespace AlmacenVencimientos.Controllers
+{
+    public class LotesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public LotesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Lotes
+        public async Task<IActionResult> Index()
+        {
+            var lotes = _context.Lotes
+                .Include(l => l.Producto)
+                .OrderBy(l => l.FechaVencimiento);
+
+            return View(await lotes.ToListAsync());
+        }
+
+        // GET: Lotes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var lote = await _context.Lotes
+                .Include(l => l.Producto)
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (lote == null)
+            {
+                return NotFound();
+            }
+
+            return View(lote);
+        }
+
+        // GET: Lotes/Create
+        public IActionResult Create()
+        {
+            CargarProductosDropDown();
+            return View();
+        }
+
+        // POST: Lotes/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,ProductoId,CodigoLote,FechaVencimiento,CantidadActual,CostoUnitario,Ubicacion")] Lote lote)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(lote);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            CargarProductosDropDown(lote.ProductoId);
+            return View(lote);
+        }
+
+        // GET: Lotes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var lote = await _context.Lotes.FindAsync(id);
+            if (lote == null)
+            {
+                return NotFound();
+            }
+
+            CargarProductosDropDown(lote.ProductoId);
+            return View(lote);
+        }
+
+        // POST: Lotes/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,ProductoId,CodigoLote,FechaVencimiento,CantidadActual,CostoUnitario,Ubicacion")] Lote lote)
+        {
+            if (id != lote.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(lote);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LoteExists(lote.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+
+            CargarProductosDropDown(lote.ProductoId);
+            return View(lote);
+        }
+
+        // GET: Lotes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var lote = await _context.Lotes
+                .Include(l => l.Producto)
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (lote == null)
+            {
+                return NotFound();
+            }
+
+            return View(lote);
+        }
+
+        // POST: Lotes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var lote = await _context.Lotes.FindAsync(id);
+            if (lote != null)
+            {
+                _context.Lotes.Remove(lote);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LoteExists(int id)
+        {
+            return _context.Lotes.Any(e => e.Id == id);
+        }
+
+        private void CargarProductosDropDown(int? productoIdSeleccionado = null)
+        {
+            var productos = _context.Productos
+                .Where(p => p.Activo)
+                .OrderBy(p => p.Nombre)
+                .ToList();
+
+            ViewData["ProductoId"] = new SelectList(productos, "Id", "Nombre", productoIdSeleccionado);
+        }
+    }
+}

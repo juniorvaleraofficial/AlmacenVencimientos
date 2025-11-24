cbd14741ebfa5c853b36b21f242d96a943d1e861
Junior Valera <detoprox@gmail.com>
2025-11-23 23:42:31 -0400

    Relación 1 - N con Lotes

--- AlmacenVencimientos/Models/Producto.cs
+++ AlmacenVencimientos/Models/Producto.cs
@@ -1,6 +1,25 @@
-﻿namespace AlmacenVencimientos.Models
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace AlmacenVencimientos.Models
 {
     public class Producto
     {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [StringLength(100)]
+        public string Nombre { get; set; } = null!;
+
+        [StringLength(100)]
+        public string? Categoria { get; set; }
+
+        [StringLength(50)]
+        public string? Codigo { get; set; }
+
+        public bool Activo { get; set; } = true;
+
+        // Relación 1 - N con Lotes
+        public ICollection<Lote> Lotes { get; set; } = new List<Lote>();
     }
 }

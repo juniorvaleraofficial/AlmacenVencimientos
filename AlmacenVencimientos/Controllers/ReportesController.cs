74641b008bc9d39656ffdf74e02fd4cb4e62ac37
Junior Valera <detoprox@gmail.com>
2025-11-24 01:31:22 -0400

    Inventario general por lote
    Productos próximos a vencer con un rango de días configurable (7, 15, 30, etc.)

---
+++ AlmacenVencimientos/Controllers/ReportesController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using AlmacenVencimientos.Data;
+using AlmacenVencimientos.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlmacenVencimientos.Controllers
+{
+    public class ReportesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ReportesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // Reporte de inventario general por lote
+        public async Task<IActionResult> Inventario()
+        {
+            var lotes = await _context.Lotes
+                .Include(l => l.Producto)
+                .OrderBy(l => l.Producto!.Nombre)
+                .ThenBy(l => l.FechaVencimiento)
+                .ToListAsync();
+
+            return View(lotes);
+        }
+
+        // Reporte de próximos a vencer (días configurables)
+        public async Task<IActionResult> ProximosVencer(int? dias)
+        {
+            int diasAviso = dias ?? 30;
+            var hoy = DateTime.Today;
+            var hasta = hoy.AddDays(diasAviso);
+
+            var lotes = await _context.Lotes
+                .Include(l => l.Producto)
+                .Where(l => l.CantidadActual > 0 &&
+                            l.FechaVencimiento >= hoy &&
+                            l.FechaVencimiento <= hasta)
+                .OrderBy(l => l.FechaVencimiento)
+                .ToListAsync();
+
+            ViewData["Dias"] = diasAviso;
+
+            return View(lotes);
+        }
+    }
+}

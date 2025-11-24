0839c05661cd234a6de22ffe424af01daf5091f2
Junior Valera <detoprox@gmail.com>
2025-11-23 23:48:02 -0400

    AppDbContext

---
+++ AlmacenVencimientos/Data/AppDbContext.cs
@@ -0,0 +1,44 @@
+﻿using AlmacenVencimientos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlmacenVencimientos.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Producto> Productos => Set<Producto>();
+        public DbSet<Lote> Lotes => Set<Lote>();
+        public DbSet<MovimientoInventario> MovimientosInventario => Set<MovimientoInventario>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Índice único opcional para el código del producto
+            modelBuilder.Entity<Producto>()
+                .HasIndex(p => p.Codigo)
+                .IsUnique(false); // cámbialo a true si quieres obligatorio y único
+
+            // Índice para código de lote
+            modelBuilder.Entity<Lote>()
+                .HasIndex(l => l.CodigoLote);
+
+            // Relaciones
+            modelBuilder.Entity<Lote>()
+                .HasOne(l => l.Producto)
+                .WithMany(p => p.Lotes)
+                .HasForeignKey(l => l.ProductoId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<MovimientoInventario>()
+                .HasOne(m => m.Lote)
+                .WithMany(l => l.Movimientos)
+                .HasForeignKey(m => m.LoteId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
